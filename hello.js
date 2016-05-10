75902ade228d0aa4b71bd01399f4e4158884b52f
Normalperson123456789 <kwaugh@connect.kellettschool.com>
2016-05-10 17:16:26 +0800

    made a game

---
+++ hello.js
@@ -0,0 +1,74 @@
+var x = 1;
+var y = 2;
+var z = 3;
+var kento = "hi ";
+
+function b() {
+  var j = prompt("what is your name");
+
+  if (j === "Kento") {
+    alert("sorry this game is temporarily closed");
+  } //end of if
+  else if (j === "lucas") {
+    alert("sorry this game is temporarily closed");
+  } //end of else if
+  else {
+    alert("welcome " + j);
+ var t = prompt("If you lose in a video game would you play again or rage");     
+    
+if (t=== "play again"){
+  alert("good answer");
+  
+   var o = prompt("what is pi?");
+  
+  if (o=== "π"){
+  alert("good answer");
+   var q = prompt(" what is 67x67?") 
+  
+  if (q=== "4489"){
+  alert("good answer");
+  
+   var g = prompt(" what is 92x63?") 
+  
+  if (g=== "5796"){
+  alert("good answer");
+   
+    var d = prompt(" what is 31x83?") 
+  
+  if (d=== "2573"){
+  alert("good answer");
+ 
+  alert("you win");
+  }
+  
+    else{alert("bad answer")}
+  }
+  
+    else{alert("bad answer")}
+  
+  }
+  
+    else{alert("bad answer")}
+  
+  }
+  
+ 
+    
+  else{ alert("i'm sorry that's not the answer i was looking for"); 
+       }
+  
+}//end of if
+else if (t=== "rage"){
+  alert("bad answer");
+  
+}//end of else if
+ 
+else{
+  alert("good answer");
+  
+}//end of else
+  
+    
+  } //end of welcome else
+
+} //end of function
