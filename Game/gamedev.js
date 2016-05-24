eb7d394f43ebf3bfedca2eb7c7bc649a99a34454
Normalperson123456789 <kwaugh@connect.kellettschool.com>
2016-05-24 16:41:55 +0800

    double check

--- Game/gamedev.js
+++ Game/gamedev.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
+var score=0 
 $("#box2").hide(); //hides apple
+
 $("#box3").hide(); //hides a thing
 $("#box4").hide(); //hides banana
 $("#box5").hide(); //hides astuff
@@ -11,7 +13,7 @@ $("#buttonkeith4").hide();
 $("#buttonkeith5").hide();
 $("#buttonkeith6").hide();
 $("#buttonnewestsofar").hide();
-
+document.write(score)
 //appear example
 
 
@@ -33,22 +35,28 @@ $("#buttonkeith").click(function() {
 $("#buttonkeith2").click(function() {
   $("#box2").show();
   $("#buttonkeith2").hide();
+  var score= score+1;
+
 });
 $("#buttonkeith3").click(function() {
   $("#box3").show();
   $("#buttonkeith3").hide();
+  var score= score+1;
 });
 $("#buttonkeith4").click(function() {
   $("#box4").show();
   $("#buttonkeith4").hide();
+  var score= score+1;
 });
 $("#buttonkeith5").click(function() {
   $("#box5").show();
   $("#buttonkeith5").hide();
+  var score= score+1;
 });
 $("#buttonkeith6").click(function() {
   $("#box6").show();
   $("#buttonkeith6").hide();
+  var score= score+1;
   alert("you win")
 });
 $("#buttonnewestsofar").click(function() {
