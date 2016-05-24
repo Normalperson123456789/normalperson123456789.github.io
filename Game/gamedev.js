7a464258132c68c0b5c37d899ba4dff1b8e72bb2
Normalperson123456789 <kwaugh@connect.kellettschool.com>
2016-05-24 15:57:43 +0800

    Added game

---
+++ Game/gamedev.js
@@ -0,0 +1,53 @@
+$( document ).ready(function() {
+$("#box2").hide(); //hides apple
+$("#box3").hide(); //hides a thing
+$("#box4").hide(); //hides banana
+$("#box5").hide(); //hides astuff
+$("#box6").hide(); //hides a button
+
+$("#buttonkeith2").hide();
+$("#buttonkeith3").hide();
+$("#buttonkeith4").hide();
+$("#buttonkeith5").hide();
+$("#buttonkeith6").hide();
+
+//appear example
+
+
+//this opens the fridge
+$("#buttonkeith").click(function() {
+  $("body").css("backgroundImage", "url(http://thumbs.dreamstime.com/x/empty-fridge-4468901.jpg)")
+  $("#buttonkeith").hide();
+  $("#box1").hide();
+  $("#button2").hide();
+  $("#button3").hide();
+  $("#button1").hide();
+  $("#buttonkeith2").show();
+  $("#buttonkeith3").show();
+  $("#buttonkeith4").show();
+  $("#buttonkeith5").show();
+  $("#buttonkeith6").show();
+});
+$("#buttonkeith2").click(function() {
+  $("#box2").show();
+  $("#buttonkeith2").hide();
+});
+$("#buttonkeith3").click(function() {
+  $("#box3").show();
+  $("#buttonkeith3").hide();
+});
+$("#buttonkeith4").click(function() {
+  $("#box4").show();
+  $("#buttonkeith4").hide();
+});
+$("#buttonkeith5").click(function() {
+  $("#box5").show();
+  $("#buttonkeith5").hide();
+});
+$("#buttonkeith6").click(function() {
+  $("#box6").show();
+  $("#buttonkeith6").hide();
+  alert("you win")
+});
+
+});
