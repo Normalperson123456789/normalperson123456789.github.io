474876a7a1ad67866a2b6592ff61827e8960b534
Normalperson123456789 <kwaugh@connect.kellettschool.com>
2016-05-30 20:33:31 +0800

    Fixed game!!! it works!!! yay!!!

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
-var x = 1
-;
+var x = 1;
+var score = 0;
 var y = 2;
 var z = 3;
 var kento = "hi ";
@@ -8,9 +8,29 @@ function b() {
   var j = prompt("what is your name");
 
   if (j === "Kento") {
+    score++;
+    score++;
+    score++;
+    score++;
+    score++;
+    score++;
+    score++;
+    score++;
+    score++;
+    score++;
     alert("You win");
   } //end of if
   else if (j === "lucas") {
+    score--;
+    score--;
+    score--;
+    score--;
+    score--;
+    score--;
+    score--;
+    score--;
+    score--;
+    score--;
     alert("sorry this game is temporarily closed");
   } //end of else if
   else {
@@ -19,67 +39,85 @@ function b() {
     
 if (t=== "play again"){
   alert("good answer");
-  
+  score++;
    var o = prompt("what is pi?");
   
   if (o=== "π"){
   alert("good answer");
+  score++;
    var q = prompt(" what is 67x67?") ;
   
   if (q=== "4489"){
   alert("good answer");
-  
+  score++;
    var g = prompt(" what is 92x63?") ;
   
   if (g=== "5796"){
   alert("good answer");
-   
+   score++;
     var d = prompt(" what is 31x83?") ;
   
   if (d=== "2573"){
   alert("good answer");
- 
+ score++;
     var fg = prompt(" what is 36x36?") ;
   
   if (fg=== "1296"){
   alert("good answer");
- 
+ score++;
  var π = prompt(" what is 29x87?") ;
   
   if (π=== "2523"){
   alert("good answer");
- 
+ score++;
+ score++;
+ score++;
+ score++;
+ score++;
+ score++;
+ score++;
+ score++;
+ score++;
+ score++;
  alert("you win");
   }
   
     else{alert("bad answer")}
+ score--;
   }
+
+ 
+  }
+  
+    else{alert("bad answer")}
+       score--;
+  }
+  
+    else{alert("bad answer")}
+       score--;
   
   }
   
     else{alert("bad answer")}
-  }
-  
-    else{alert("bad answer")}
-  
-  }
-  
-    else{alert("bad answer")}
-  
+   score--;
   }
   
  
     
   else{ alert("i'm sorry that's not the answer i was looking for"); 
+        score--;
        }
+
   
 }//end of if
 else if (t=== "rage"){
+   score--;
   alert("bad answer");
   
 }//end of else if
  
 else{
+   score--;
   alert("bad answer");
   
 }//end of else
@@ -88,3 +126,193 @@ else{
   } //end of welcome else
 
 } //end of function
+
+
+
+
+function pi() {
+
+alert("welcome");
+
+    var a = prompt("How many digits of pi can you remember after 3.?"); 
+
+if (a === "1") {
+   score++;
+var b = prompt (" ");
+
+if (b === "4"){
+ score++;
+var c = prompt(" ");
+ 
+ if (c === "1"){
+ score++;
+ var d = prompt(" ");
+ 
+ if (d === "5"){
+ score++;
+var e = prompt(" ");
+ 
+ if (e === "9"){
+ score++;
+ var f = prompt(" ");
+ 
+ if (f === "2"){
+ score++;
+ var g = prompt(" ");
+ 
+ if (g === "6"){
+ score++;
+ var h = prompt(" ");
+ 
+ if (h === "5"){
+ score++;
+ var i = prompt(" ");
+ 
+ if (i === "3"){
+ score++;
+ var j = prompt(" ");
+ 
+ if (j === "5"){
+ score++;
+ var k = prompt(" ");
+ 
+ if (k === "8"){
+ score++;
+ var l = prompt(" ");
+ 
+ if (l === "9"){
+ score++;
+ var m = prompt(" ");
+ 
+ if (m === "7"){
+ score++;
+ var n = prompt(" ");
+ 
+ if (n === "9"){
+ score++;
+  var o = prompt(" ");
+ 
+ if (o === "3"){
+ score++;
+var p = prompt(" ");
+ 
+ if (p === "2"){
+ score++;
+var q = prompt(" ");
+ 
+ if (q === "3"){
+ score++;
+ 
+
+ }
+else{
+  score--;
+  alert("you lose");
+}   
+ }
+else{
+   score--;
+  alert("you lose");
+}   
+ }
+else{
+   score--;
+  alert("you lose");
+} 
+ }
+else{
+   score--;
+  alert("you lose");
+} 
+ }
+else{
+   score--;
+  alert("you lose");
+} 
+ }
+else{
+   score--;
+  alert("you lose");
+} 
+ }
+else{ 
+score--;
+
+  alert("you lose");
+}
+
+ }
+else{
+  score--;
+  alert("you lose");
+} 
+ }
+else{
+  score--;
+  alert("you lose");
+} 
+ }
+else{
+  score--;
+  alert("you lose");
+} 
+ }
+else{
+  score--;
+  alert("you lose");
+} 
+ }
+else{
+  score--;
+  alert("you lose");
+} 
+ }
+else{
+  score--;
+  alert("you lose");
+}   
+ }
+else{
+  score--;
+  alert("you lose");
+} 
+ }
+else{
+  score--;
+  alert("you lose");
+}
+}
+else{
+  score--;
+  alert("you lose");
+}
+
+}
+
+else {
+  score--;
+  alert("you lose");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+function score()
