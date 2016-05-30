7eb569d112cad13642ab40977a6cb165c88f0751
Normalperson123456789 <kwaugh@connect.kellettschool.com>
2016-05-30 19:47:25 +0800

    Made another game

---
+++ hello2.js
@@ -0,0 +1,164 @@
+function pi() {
+alert("welcome")
+
+var a = prompt("How many digits of pi can you remember after 3.?");	
+
+if (a === 1) {
+var b = prompt (" ");
+
+if (b === 4){
+
+var c = prompt(" ");
+ 
+ if (c === 1){
+
+ var d = prompt(" ");
+ 
+ if (d === 5){
+
+var e = prompt(" ");
+ 
+ if (e === 9){
+
+ var f = prompt(" ");
+ 
+ if (f === 2){
+
+ var g = prompt(" ");
+ 
+ if (g === 6){
+
+ var h = prompt(" ");
+ 
+ if (h === 5){
+
+ var i = prompt(" ");
+ 
+ if (i === 3){
+
+ var j = prompt(" ");
+ 
+ if (j === 5){
+
+ var k = prompt(" ");
+ 
+ if (k === 8){
+
+ var l = prompt(" ");
+ 
+ if (l === 9){
+
+ var m = prompt(" ");
+ 
+ if (m === 7){
+
+ var n = prompt(" ");
+ 
+ if (n === 9){
+
+  var o = prompt(" ");
+ 
+ if (o === 3){
+
+var p = prompt(" ");
+ 
+ if (p === 2){
+
+var q = prompt(" ");
+ 
+ if (q === 3){
+
+ 
+
+ }
+else{
+	alert("you lose")
+} 	
+ }
+else{
+	alert("you lose")
+} 	
+ }
+else{
+	alert("you lose")
+}	
+ }
+else{
+	alert("you lose")
+}	
+ }
+else{
+	alert("you lose")
+}	
+ }
+else{
+	alert("you lose")
+}	
+ }
+else{
+	alert("you lose")
+}
+
+ }
+else{
+	alert("you lose")
+}	
+ }
+else{
+	alert("you lose")
+}	
+ }
+else{
+	alert("you lose")
+}	
+ }
+else{
+	alert("you lose")
+}	
+ }
+else{
+	alert("you lose")
+}	
+ }
+else{
+	alert("you lose")
+} 	
+ }
+else{
+	alert("you lose")
+}	
+ }
+else{
+	alert("you lose")
+}
+}
+else{
+	alert("you lose")
+}
+
+}
+
+else {
+	alert("you lose");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
